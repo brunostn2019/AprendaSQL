e4d097c68191ceec6ded9b8700440eb1685407e5
brunostn2019 <bruno.lico@gmail.com>
2021-07-01 09:50:14 -0300

    retirando banco mysql

--- src/main/java/com/sqlpt/sqlptbr/Controller/SqlptController.java
+++ src/main/java/com/sqlpt/sqlptbr/Controller/SqlptController.java
@@ -1,64 +1,13 @@
 package com.sqlpt.sqlptbr.Controller;
 
-import com.sqlpt.sqlptbr.Service.SqlptService;
-import com.sqlpt.sqlptbr.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.validation.Valid;
-import java.time.LocalDate;
-import java.util.List;
 
 @Controller
 public class SqlptController {
 
-    @Autowired
-    SqlptService sqlptService;
-
-
-
-    //region [BLOG POST METODOS]
-    @RequestMapping(value = "/posts", method = RequestMethod.GET)
-    public ModelAndView getPosts() {
-        ModelAndView mv = new ModelAndView("posts");
-        List<Post> posts = sqlptService.findAll();
-        mv.addObject("posts", posts);
-        return mv;
-    }
-
-    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
-    public ModelAndView getPostDetails(@PathVariable("id") Long id) {
-        ModelAndView mv = new ModelAndView("postDetails");
-        Post post = sqlptService.findById(id);
-        mv.addObject("post", post);
-        return mv;
-    }
-
-    @RequestMapping(value="/newpost",method = RequestMethod.GET)
-    public String getPostForm()
-    {
-        return "postForm";
-    }
-
-    @RequestMapping(value="/newpost",method = RequestMethod.POST)
-    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes)
-    {
-        if(result.hasErrors()) {
-            attributes.addFlashAttribute("mensagem","Verifique os campos obrigatórios!");
-            return "redirect:/newpost";
-        }
-        post.setData(LocalDate.now());
-        sqlptService.save(post);
-        return "redirect:/posts";
-    }
-    //endregion
 
     //region [PAGINAS SQL]
 
--- src/main/java/com/sqlpt/sqlptbr/Repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sqlpt.sqlptbr.Repository;
-
-import com.sqlpt.sqlptbr.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post,Long> {
-
-
-}
